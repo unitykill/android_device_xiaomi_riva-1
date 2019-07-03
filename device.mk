80b964dba83d2fca8592abadfb7b515fb7e3a545
Nick <nikhilnick00007@gmail.com>
2019-07-03 08:53:09 +0000

    riva: Add dirac support Squash of commits
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- device.mk
+++ device.mk
@@ -43,10 +43,6 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# Ambient display
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
@@ -111,6 +107,10 @@ PRODUCT_PACKAGES += \
 
 # Charger
 include $(LOCAL_PATH)/rootdir/charger/charger.mk
+
+# Device-specific Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
 
 # Display
 PRODUCT_PACKAGES += \
